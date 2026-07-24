7f8649147e33861d002a282922e66a7158abd845
rpsy0786-dot <rpsy0786@gmail.com>
2026-07-24 22:44:29 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,15 @@
+import sqlite3
 
+conn = sqlite3.connect("jobs.db")
+
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS jobs(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+title TEXT,
+link TEXT UNIQUE
+)
+""")
+
+conn.commit()
