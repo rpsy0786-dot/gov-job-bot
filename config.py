02def2d0a734d5f6f59ec68b00f379871bc76740
rpsy0786-dot <rpsy0786@gmail.com>
2026-07-24 22:39:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+BOT_TOKEN = "8700453614:AAFdsilRW8WeeOOwtVnYVj7rptBm8HjtLFA"
 
+CHAT_ID = "1286878337"
+
+KEYWORDS = [
+    "Mechanical",
+    "Mechanical Engineer",
+    "Maintenance Engineer",
+    "Assistant Engineer",
+    "Executive Engineer",
+    "Junior Engineer",
+    "Plant Maintenance",
+    "Refinery",
+    "Power Plant",
+    "Rotating Equipment",
+    "Static Equipment"
+]
