0befc82eb9f57ab89a77bffcd49dfb63db799b2c
rpsy0786-dot <rpsy0786@gmail.com>
2026-07-24 22:43:25 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,10 @@
+from telegram import Bot
+from config import BOT_TOKEN, CHAT_ID
 
+bot = Bot(BOT_TOKEN)
+
+async def send_message(msg):
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text=msg
+    )
